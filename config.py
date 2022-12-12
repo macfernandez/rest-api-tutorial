70e2afe09b157e3fdd51f0e3baee486b20219313
Macarena Fernandez Urquiza <murquiza@uma-health.com>
2022-12-12 10:50:39 -0300

    add config for db

---
+++ config.py
@@ -0,0 +1,22 @@
+import os
+#import pathlib
+import connexion
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+
+basedir = os.path.abspath('.')
+#basedir = pathlib.Path(__name__).parent.resolve()
+connex_app = connexion.App(__name__, specification_dir=basedir)
+
+app = connex_app.app
+app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{basedir}/people.db"
+# turns the SQLAlchemy event system off
+# TODO
+# read https://docs.sqlalchemy.org/en/14/core/event.html
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+# TODO
+# https://flask-sqlalchemy.palletsprojects.com/en/3.0.x/config/
+ma = Marshmallow(app)
