0ac7fc1d7ac4a0ae0d7a170df039562c8fd81a91
Macarena Fernandez Urquiza <murquiza@uma-health.com>
2022-12-14 18:59:00 -0300

    improve readme and delete unnecessay comments in src code

--- src/config.py
+++ src/config.py
@@ -9,13 +9,7 @@ connex_app = connexion.App(__name__, specification_dir=basedir)
 
 app = connex_app.app
 app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{basedir}/people.db"
-# turns the SQLAlchemy event system off
-# TODO
-# read https://docs.sqlalchemy.org/en/14/core/event.html
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
-
-# TODO
-# https://flask-sqlalchemy.palletsprojects.com/en/3.0.x/config/
 ma = Marshmallow(app)
