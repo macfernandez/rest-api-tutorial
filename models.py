6db7e641d6c801cbb8dd92ae27850cba8e652de4
Macarena Fernandez Urquiza <murquiza@uma-health.com>
2022-12-14 10:26:32 -0300

    add note model and schema

--- models.py
+++ models.py
@@ -1,6 +1,26 @@
-from config import db, ma
 from datetime import datetime
+from marshmallow_sqlalchemy import fields
 
+from config import db, ma
+
+
+class Note(db.Model):
+    __tablename__ = "note"
+    id = db.Column(db.Integer, primary_key=True)
+    person_id = db.Column(db.Integer, db.ForeignKey("person.id"))
+    content = db.Column(db.String, nullable=False)
+    timestamp = db.Column(
+        db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow
+    )
+
+
+class NoteSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Note
+        load_instance = True
+        sqla_session = db.session
+        include_fk = True
+    
 
 class Person(db.Model):
     __tablename__ = "person"
@@ -10,6 +30,13 @@ class Person(db.Model):
     timestamp = db.Column(
         db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow
     )
+    notes = db.relationship(
+        Note,
+        backref="person",
+        cascade="all, delete, delete-orphan",
+        single_parent=True,
+        order_by="desc(Note.timestamp)"
+    )
 
 
 class PersonSchema(ma.SQLAlchemyAutoSchema):
@@ -17,6 +44,11 @@ class PersonSchema(ma.SQLAlchemyAutoSchema):
         model = Person
         load_instance = True
         sqla_session = db.session
+        include_relationships = True
+
+    notes = fields.Nested(NoteSchema, many=True)
+    
 
 person_schema = PersonSchema()
 people_schema = PersonSchema(many=True)
+note_schema = NoteSchema()
